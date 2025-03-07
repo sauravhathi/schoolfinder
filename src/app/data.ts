88948ac9eef602d79698e7d2deffd0725bac0752
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2025-03-08 01:39:48 +0530

    Committing src/app/data.ts

---
+++ src/app/data.ts
@@ -0,0 +1,134 @@
+export const columns = [
+    {
+        Header: "Id",
+        accessor: "id",
+    },
+    {
+        Header: "UDISE Code",
+        accessor: "udiseschcode",
+    },
+    {
+        Header: "University Name",
+        accessor: "universityname",
+    },
+    {
+        Header: "University ID",
+        accessor: "universityid",
+    },
+    {
+        Header: "University Type",
+        accessor: "universitytype",
+    },
+    {
+        Header: "College Name",
+        accessor: "collegename",
+    },
+    {
+        Header: "College ID",
+        accessor: "collegeid",
+    },
+    {
+        Header: "College Type",
+        accessor: "collegetype",
+    },
+    {
+        Header: "Institution Name",
+        accessor: "institutionname",
+    },
+    {
+        Header: "Institution ID",
+        accessor: "institutionid",
+    },
+    {
+        Header: "Institution Type",
+        accessor: "institutiontype",
+    },
+    {
+        Header: "Address",
+        accessor: "address",
+    },
+    {
+        Header: "Website",
+        accessor: "website",
+    },
+    {
+        Header: "Specialised In",
+        accessor: "specialisedin",
+    },
+    {
+        Header: "School Name",
+        accessor: "schoolname",
+    },
+    {
+        Header: "School Type",
+        accessor: "schooltype",
+    },
+    {
+        Header: "Pincode",
+        accessor: "pincode",
+    },
+    {
+        Header: "Locality",
+        accessor: "locality",
+    },
+    {
+        Header: "Category",
+        accessor: "category",
+    },
+    {
+        Header: "Type",
+        accessor: "type",
+    },
+    {
+        Header: "Class From",
+        accessor: "classfrom",
+    },
+    {
+        Header: "Class To",
+        accessor: "classto",
+    },
+    {
+        Header: "Status",
+        accessor: "status",
+    },
+    {
+        Header: "State",
+        accessor: "state",
+    },
+    {
+        Header: "District",
+        accessor: "district",
+    },
+    {
+        Header: "Block",
+        accessor: "block",
+    },
+    {
+        Header: "Cluster",
+        accessor: "cluster",
+    },
+    {
+        Header: "Village",
+        accessor: "village",
+    },
+    {
+        Header: "Management",
+        accessor: "management",
+    },
+    {
+        Header: "Year of Establishment",
+        accessor: "yearofestablishment",
+    },
+    {
+        Header: "Year When Decalred University",
+        accessor: "yearwhendecalreduniversity",
+    },
+    {
+        Header: "Upload Year",
+        accessor: "uploadyear",
+    },
+    {
+        Header: "Institute Added In Survey Year",
+        accessor: "instituteaddedinsurveyyear",
+    },
+]
